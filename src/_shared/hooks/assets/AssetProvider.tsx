3930306abb70b3c3698be49b13f44fe62033653b
Mahmoud Okily <heshamalamam73@gmail.com>
2022-07-12 21:39:58 +0200

    add docker files for productions

--- src/_shared/hooks/assets/AssetProvider.tsx
+++ src/_shared/hooks/assets/AssetProvider.tsx
@@ -1,22 +1,22 @@
-import React, { createContext } from 'react';
-import { AssetDomains } from './assets';
+import React, { createContext } from "react";
+import { AssetDomains } from "./assets";
 
 type Props = {
-    assets?: AssetDomains;
-    endpoint?: string;
-    children: any;
+  assets?: AssetDomains;
+  endpoint?: string;
+  children: any;
 };
 
 const AssetContext = createContext<AssetDomains>({
-    icons: {},
-    images: {},
-    svg: {}
+  icons: {},
+  images: {},
+  svg: {},
 });
 
 const { Provider } = AssetContext;
 
 const AssetProvider: React.FC<Props> = ({ assets, children }) => {
-    return <Provider value={assets as AssetDomains}>{children}</Provider>;
+  return <Provider value={assets as AssetDomains}>{children}</Provider>;
 };
 
 export { AssetContext, AssetProvider };
