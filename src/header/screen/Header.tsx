2a81a666af33038409058b893f0b3338a55a061a
Mahmoud Okily <heshamalamam73@gmail.com>
2022-07-16 09:53:12 +0200

    feat(general): fix header and footer compoenents

--- src/header/screen/Header.tsx
+++ src/header/screen/Header.tsx
@@ -1,5 +1,71 @@
-import Navbar from "../components/Navbar";
+import { useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import {
+  MobileIcon,
+  Nav,
+  NavbarContainer,
+  NavIcon,
+  NavItem,
+  NavLinks,
+  NavLogo,
+  NavMenu,
+} from "../../_shared/styledComponents";
+import Svg from "../../_shared/styledComponents/Svg";
+import links from "../../_shared/utils/data/links";
+
 const Header = () => {
-  return <Navbar />;
+  const [show, setShow] = useState(false);
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    setShow(!show);
+  };
+
+  const scrollTo = (id: string) => {
+    const element = document.getElementById(id);
+
+    element?.scrollIntoView({
+      behavior: "smooth",
+    });
+  };
+
+  const closeMobileMenu = (to?: string, id?: string, action?: string) => {
+    if (id && location.pathname === "/") {
+      scrollTo(id);
+    }
+    if (to) {
+      navigate(to);
+    }
+
+    setShow(false);
+  };
+
+  return (
+    <Nav>
+      <NavbarContainer>
+        <NavLogo to="/">
+          <NavIcon src="./assets/logo.svg" alt="logo" />
+          TechIt
+        </NavLogo>
+        <MobileIcon onClick={handleClick}>
+          {show ? (
+            <Svg name="Close" color="white" />
+          ) : (
+            <Svg name="Dashboard" color="white" />
+          )}
+        </MobileIcon>
+        <NavMenu show={show}>
+          {links.map((el, index) => (
+            <NavItem key={index}>
+              <NavLinks onClick={() => closeMobileMenu(el?.to, el.id)}>
+                {el.text}
+              </NavLinks>
+            </NavItem>
+          ))}
+        </NavMenu>
+      </NavbarContainer>
+    </Nav>
+  );
 };
 export default Header;
