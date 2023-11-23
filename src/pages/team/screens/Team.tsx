9e2a5c04a03bfe74733840cd5dbf574155c0f100
mahmoudokily <mahmoudokily93@gmail.com>
2023-11-23 15:02:49 +0100

    add team

--- src/pages/team/screens/Team.tsx
+++ src/pages/team/screens/Team.tsx
@@ -1,43 +1,47 @@
+import { FaGithub, FaLinkedin } from "react-icons/fa";
 import { Box, Button, Card, Flex, Typography } from "../../../_shared/UI";
 import { PageLayout } from "../../../_shared/layouts/PageLayout";
+import { FooterSocialIcon, Row } from "../../../_shared/styledComponents";
+import { footerSocialData } from "../../../_shared/utils/data/footer";
+import { ReactNode } from "react";
 
 const team = [
   {
     name: "Mahmoud Okily",
     role: "Partner",
-    otherRoles: ["Senior Software Developer", "team leader"],
-    image: require("../../../_shared/assets/mido.jpg"),
+    otherRoles: ["Senior Software Developer", "Team leader"],
+    image: require("../../../_shared/assets/mido2.png"),
+    social: [
+      {
+        name: "linkedin",
+        link: "https://www.linkedin.com/in/mahmoudokily/",
+        icon: <FaLinkedin color="white" />,
+      },
+      {
+        name: "github",
+        link: "https://github.com/mahmoudokily",
+        icon: <FaGithub color="white" />,
+      },
+    ],
   },
   {
     name: "Boclair Temgoua",
     role: "Partner",
-    otherRoles: ["Senior Software Developer", "team leader"],
-    image: require("../../../_shared/assets/who.jpg"),
+    otherRoles: ["Senior Software Developer", "Team leader"],
+    image: require("../../../_shared/assets/bop.jpg"),
+    social: [
+      {
+        link: "https://www.linkedin.com/in/boclair-temgoua-688b42b8/",
+        name: "linkedin",
+        icon: <FaLinkedin color="white" />,
+      },
+      {
+        link: "https://github.com/boclair-temgoua",
+        name: "github",
+        icon: <FaGithub color="white" />,
+      },
+    ],
   },
-  // {
-  //   name: "Mahmoud Okily",
-  //   role: "CEO",
-  //   otherRoles: ["Senior Software Developer", "team leader"],
-  //   image: require("../../../_shared/assets/mido.jpg"),
-  // },
-  // {
-  //   name: "Mahmoud Okily",
-  //   role: "CEO",
-  //   otherRoles: ["Senior Software Developer", "team leader"],
-  //   image: require("../../../_shared/assets/mido.jpg"),
-  // },
-  // {
-  //   name: "Mahmoud Okily",
-  //   role: "CEO",
-  //   otherRoles: ["Senior Software Developer", "team leader"],
-  //   image: require("../../../_shared/assets/mido.jpg"),
-  // },
-  // {
-  //   name: "Mahmoud Okily",
-  //   role: "CEO",
-  //   otherRoles: ["Senior Software Developer", "team leader"],
-  //   image: require("../../../_shared/assets/mido.jpg"),
-  // },
 ];
 const Team = () => {
   return (
@@ -55,18 +59,59 @@ const Team = () => {
         >
           {team.map((person) => {
             return (
-              <Card overflow={"hidden"} width={300} minHeight={350}>
-                <Flex>
-                  <img alt="mahmoud okily" src={person.image} />
+              <Card overflow={"hidden"} width={300} minHeight={300}>
+                <Flex justifyContent={"center"} alignItems={"center"} p={3}>
+                  <img
+                    alt="mahmoud okily"
+                    src={person.image}
+                    width={200}
+                    height={200}
+                    style={{ borderRadius: "50%", border: "1px solid #003eaa" }}
+                  />
                 </Flex>
-                <Flex p={3} flexDirection={"column"} style={{ gap: "5px 5px" }}>
-                  <Typography variant={"title30"}>{person.name}</Typography>
-                  <Typography fontWeight={"bold"} variant={"title10"}>
+                <Flex
+                  p={3}
+                  flexDirection={"column"}
+                  style={{ gap: "5px 5px" }}
+                  flexShrink={0}
+                  height={200}
+                  borderTop={"1px solid #003eaa"}
+                  bg="primary"
+                >
+                  <Typography color="white" variant={"title30"}>
+                    {person.name}
+                  </Typography>
+                  <Typography
+                    color="white"
+                    fontWeight={"bold"}
+                    variant={"title10"}
+                  >
                     {person.role}
                   </Typography>
                   {person.otherRoles.map((role) => {
-                    return <Typography variant={"body10"}>{role}</Typography>;
+                    return (
+                      <Typography color="white" variant={"body10"}>
+                        {role}
+                      </Typography>
+                    );
                   })}
+                  <Box mt={2}>
+                    <Button disabled $size="default" block variant="light">
+                      Download Cv
+                    </Button>
+                  </Box>
+                  <Row alignItems="center" margin="10px  0 0 0" gridGap="1rem">
+                    {person.social.map((social: any, index: number) => (
+                      <FooterSocialIcon
+                        key={index}
+                        href={social.link}
+                        target="_blank"
+                        aria-label={social.name}
+                      >
+                        {social.icon}
+                      </FooterSocialIcon>
+                    ))}
+                  </Row>
                 </Flex>
               </Card>
             );
